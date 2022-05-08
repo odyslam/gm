1dd157ed6ee2493e994714b96c0f7efa113fa051
Odysseas Lamtzidis <odyslam@gmail.com>
2022-05-08 12:40:13 +0300

    chore: skip prompt for rust installation

--- install-software-common.sh
+++ install-software-common.sh
@@ -3,7 +3,7 @@ install_software_common(){
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
   message "Installing Rust.. via Rustup"
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs |sh -s -- -y
 
   message "Installing Typescript (globally).."
   npm install -g typescript
