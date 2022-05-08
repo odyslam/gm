3a64e733e6188b386039ccff7900a46b9becebaf
Odysseas Lamtzidis <odyslam@gmail.com>
2022-05-08 08:32:12 +0300

    Add system setup messages

---
+++ system-setup.sh
@@ -0,0 +1,9 @@
+system_setup_mac(){
+  message "Set caps lock to control ⌃ and vice-versa"
+  message "Turn On 'Do Not Disturb"
+  message "Increase touchpad sensitivity to maximum"
+}
+
+system_setup_linux(){
+  warning "System setup for Linux is still TBD"
+}
