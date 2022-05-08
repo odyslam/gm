fea73ced91fdfe3ff53a0689158b867c64562017
Odysseas Lamtzidis <odyslam@gmail.com>
2022-05-08 12:02:23 +0300

    feat: install powerfile font

--- install-software-common.sh
+++ install-software-common.sh
@@ -12,6 +12,15 @@ install_software_common(){
   message "Installing tranmissions11/headers.."
   git clone https://github.com/transmissions11/headers
   cd headers && cargo build --release
-  cp target/release/headers ~/.cargo/headers
+  cp target/release/headers ~/.cargo/bin/headers
   cd .. && rm -rf headers
+
+  message "Installing Powerfiline Fonts.."
+  git clone https://github.com/powerline/fonts.git --depth=1
+  # install
+  cd fonts
+  ./install.sh
+  # clean-up a bit
+  cd ..
+  rm -rf fonts
 }
